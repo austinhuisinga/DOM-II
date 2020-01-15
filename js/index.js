a793b25576f2b298144f4c539f590417d84ae906
austinhuisinga <57517058+austinhuisinga@users.noreply.github.com>
2020-01-14 19:17:38 -0700

    MVP complete.

--- js/index.js
+++ js/index.js
@@ -3,7 +3,11 @@
 const button = document.querySelectorAll('.btn');
 const link = document.querySelectorAll('.nav-link');
 const logo = document.querySelector('.logo-heading');
+const allImg = document.querySelectorAll('img');
+// const stopLink = document.querySelector('.nav-link');
+const destination = document.querySelectorAll('.destination');
 
+const stopLink = document.querySelector('.nav-link');
 // event listeners
 button.forEach(element => {
   element.addEventListener('mouseenter', () => {
@@ -19,11 +23,9 @@ button.forEach(element => {
 link.forEach(element => {
   element.addEventListener('mouseenter', () => {
     element.style.color = '#C0C0C0';
-    // element.style.textDecoration = 'underline';
   })
   element.addEventListener('mouseleave', () => {
     element.style.color = '#212529';
-    // element.style.textDecoration = 'none';
   })
 })
 
@@ -45,4 +47,44 @@ logo.addEventListener('mouseenter', () => {
 logo.addEventListener('mouseleave', () => {
   logo.style.color = '#212529';
   // logo.style.transition = '5000ms';
+})
+
+allImg.forEach(event => {
+  event.style.border = '1px solid transparent';
+  event.style.borderRadius = '10px';
+  event.addEventListener('mouseenter', () => {
+    event.style.transform = 'scale(1.1)';
+    event.style.transition = '400ms';
+  })
+  event.addEventListener('mouseleave', () => {
+    event.style.transform = 'scale(1)';
+  })
+})
+
+// stopLink.forEach((event) => {
+//   event.addEventListener('click', () => {
+//     event.preventDefault();
+//   })
+// })
+
+// contentPick.addEventListener('click', () => {
+//   // contentPick.style.transform = 'scale(1.2)';
+//   contentPick.style.backgroundColor = 'red';
+// })
+// contentPick.addEventListener('mouseenter', () => {
+//   contentPick.style.backgroundColor = 'red';
+// })
+// contentPick.style.backgroundColor = 'red';
+
+stopLink.addEventListener('click', (event) => {
+  event.preventDefault();
+})
+
+destination.forEach(event => {
+  event.addEventListener('mouseenter', () => {
+    event.style.transform = 'scale(1.1)';
+  })
+  event.addEventListener('mouseleave', () => {
+    event.style.transform = 'scale(1)';
+  })
 })
